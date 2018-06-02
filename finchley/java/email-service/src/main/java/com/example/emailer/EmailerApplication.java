8835e076880fb910d7b2cdd9ae691d4b1bc33a21
Josh Long <starbuxman@gmail.com>
2018-06-02 17:21:52 +0800

    emailer

--- finchley/java/email-service/src/main/java/com/example/emailer/EmailerApplication.java
+++ finchley/java/email-service/src/main/java/com/example/emailer/EmailerApplication.java
@@ -1,12 +1,15 @@
 package com.example.emailer;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.function.Function;
 
@@ -15,19 +18,22 @@ public class EmailerApplication {
 
 		private final Log log = LogFactory.getLog(getClass());
 
-		private final RestTemplate restTemplate = new RestTemplate();
+		@Bean
+		WebClient client(LoadBalancerExchangeFilterFunction eff) {
+				return WebClient.builder().filter(eff).build();
+		}
 
 		@Bean
-		Function<String, String> email(Environment env) {
+		Function<String, String> email(WebClient client) {
 				return input -> {
-/*
 
-						String json = this.restTemplate.getForObject("http://localhost:8888/email-service/default", String.class);
-						this.log.info("json: " + json);
-*/
+						client
+							.get()
+							.uri("http://reservation-service/reservations")
+							.retrieve()
+							.bodyToFlux(Reservation.class)
+							.subscribe(System.out::println);
 
-						String msgFromConfigService = env.getProperty("message");
-						this.log.info("message: " + msgFromConfigService);
 						return input;
 				};
 		}
@@ -35,4 +41,11 @@ public class EmailerApplication {
 		public static void main(String[] args) {
 				SpringApplication.run(EmailerApplication.class, args);
 		}
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Reservation {
+		private String id, email;
 }
