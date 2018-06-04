0cced3b551a342078a104eda91e7bce1103f51bd
Josh Long <starbuxman@gmail.com>
2018-06-04 00:08:17 -0700

    publish email

--- finchley/java/email-service/go.sh
+++ finchley/java/email-service/go.sh
@@ -10,6 +10,11 @@ riff delete -n emailer --all
 # logical name
 app=emailer
 
+kubectl delete secret sendgrid-api-key
+echo $SENDGRID_API_KEY > api-key.txt
+kubectl create secret generic sendgrid-api-key --from-file=sendgrid-api-key=./api-key.txt
+
+
 # deploy 
 riff create java -a target/${app}-0.0.1-SNAPSHOT.jar -i $app -n $app --handler "email"
 
--- finchley/java/email-service/src/main/java/com/example/emailer/EmailerApplication.java
+++ finchley/java/email-service/src/main/java/com/example/emailer/EmailerApplication.java
@@ -1,12 +1,12 @@
 package com.example.emailer;
 
+import com.sendgrid.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction;
@@ -21,54 +21,47 @@ public class EmailerApplication {
 
 		private final Log log = LogFactory.getLog(getClass());
 
-		@Bean
-		ApplicationRunner run(@Value("${message}") String msg) {
-				return args -> this.log.info("message: " + msg);
+		public EmailerApplication(@Value("${message}") String msg) {
+				log.info("config server message: " + msg);
 		}
 
 		@Bean
 		WebClient client(LoadBalancerExchangeFilterFunction eff) {
 				return WebClient.builder().filter(eff).build();
 		}
-/*
+
 
 		@Bean
-		SendGrid sendGrid(@Value("${SENDGRID_API_KEY}") String key) {
-				return new SendGrid(key);
-		}
-*/
+		Function<EmailRequest, Boolean> email(WebClient client, SendGrid sg) {
+				return input -> {
+						return
+							client
+								.get()
+								.uri("http://reservation-service/reservations")
+								.retrieve()
+								.bodyToFlux(Reservation.class)
+								.filter(x -> x.getId().equals(input.getReservationId()))
+								.flatMap(r -> {
+										try {
 
-		@Bean
-		Function<Flux<EmailRequest>, Flux<Boolean>> email(WebClient client/*, SendGrid sg*/) {
-				return input ->
-					input
-						.flatMap(i -> client
-							.get()
-							.uri("http://reservation-service/reservations")
-							.retrieve()
-							.bodyToFlux(Reservation.class)
-							.filter(x -> x.getId().equals(i.getReservationId()))
-							.flatMap(r -> {
-									try {
-											/*
-											Email from = new Email("spring-tips@joshlong.com");
-											String subject = "Bootiful Riff";
-											Email to = new Email(r.getEmail());
-											Content content = new Content("text/plain", "Hello world!");
-											Mail mail = new Mail(from, subject, to, content);
-											Request request1 = new Request();
-											request1.setMethod(Method.POST);
-											request1.setEndpoint("mail/send");
-											request1.setBody(mail.build());
-											Response response = sg.api(request1);
-											*/
-											return Flux
-												.just(true); // (response.getStatusCode() >= 200 && response.getStatusCode() < 300));
-									}
-									catch (Exception e) {
-											throw new RuntimeException(e);
-									}
-							}));
+												Email from = new Email("spring-tips@joshlong.com");
+												String subject = "Bootiful Riff";
+												Email to = new Email(r.getEmail());
+												Content content = new Content("text/plain", "Hello world!");
+												Mail mail = new Mail(from, subject, to, content);
+												Request request1 = new Request();
+												request1.setMethod(Method.POST);
+												request1.setEndpoint("mail/send");
+												request1.setBody(mail.build());
+												Response response = sg.api(request1);
+												return Flux.just(true); // (response.getStatusCode() >= 200 && response.getStatusCode() < 300));
+										}
+										catch (Exception e) {
+												throw new RuntimeException(e);
+										}
+								})
+								.blockFirst();
+				};
 		}
 
 
