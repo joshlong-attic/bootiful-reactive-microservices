d021d9c77bad0241304cb0886625bf0dcf3f958c
Josh Long <josh@joshlong.com>
2018-10-09 11:48:10 -0700

    now with RSocket.

--- finchley/java/reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
+++ finchley/java/reservation-client/src/main/java/com/example/reservationclient/ReservationClientApplication.java
@@ -1,26 +1,32 @@
 package com.example.reservationclient;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.rsocket.Payload;
+import io.rsocket.RSocketFactory;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.util.DefaultPayload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancerExchangeFilterFunction;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.cloud.netflix.hystrix.HystrixCommands;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.annotation.Id;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
@@ -28,91 +34,139 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @SpringBootApplication
 public class ReservationClientApplication {
 
-		@Bean
-		RouterFunction<ServerResponse> routes(WebClient client) {
-				return route(GET("/reservations/names"), r -> {
 
-						Publisher<String> map = client
-							.get()
-							.uri("http://reservation-service/reservations")
-							.retrieve()
-							.bodyToFlux(Reservation.class)
-							.map(Reservation::getEmail);
+	@Bean
+	@LoadBalanced
+	WebClient webClient(WebClient.Builder builder) {
+		return builder.build();
+	}
 
-						Publisher<String> fallback =
-							HystrixCommands
-								.from(map)
-								.fallback(Mono.just("EEK!"))
-								.commandName("fallback")
-								.eager()
-								.build();
+	@Bean
+	RouterFunction<ServerResponse> routes(ReservationClient client) {
+		return route(GET("/reservations/names"),
+			request -> {
+/*
+				Flux<String> stringFlux = client
+					.get()
+					.uri("http://reservation-service/reservations")
+					.retrieve()
+					.bodyToFlux(Reservation.class)
+					.map(Reservation::getReservationName);*/
 
-						return ServerResponse.ok().body(fallback, String.class);
-				});
-		}
+				Flux<String> allReservations = client.getAllReservations()
+					.map(r -> r.getReservationName());
 
-		@Bean
-		MapReactiveUserDetailsService authentication() {
-				return new MapReactiveUserDetailsService(
-					User.withDefaultPasswordEncoder().username("user").password("pw").roles("USER").build()
-				);
-		}
-	
-		@Bean
-		SecurityWebFilterChain authorization(ServerHttpSecurity security) {
-				//@formatter:off
-				return security
-					.csrf().disable()
-					.httpBasic()
-					.and()
-					.authorizeExchange()
-							.pathMatchers("/proxy").authenticated()
-							.anyExchange().permitAll()
-					.and()
+				/*Publisher<String> stringPublisher = HystrixCommands
+					.from(stringFalux)
+					.eager()
+					.commandName("names")
+					.fallback(Flux.just("EEK!"))
 					.build();
-				//@formatter:on
-		}
 
-		@Bean
-		RedisRateLimiter redisRateLimiter() {
-				return new RedisRateLimiter(5, 7);
-		}
+				*/
+				return ServerResponse.ok().body(allReservations, String.class);
+			});
+	}
 
-	
-	
-	
-  @Bean
-  WebClient client(LoadBalancerExchangeFilterFunction eff) {
-    return WebClient.builder().filter(eff).build();
-  }
-	
-	
-	
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationClientApplication.class, args);
+	}
 
-		@Bean
-		RouteLocator gateway(RouteLocatorBuilder rlb) {
-				return
-					rlb
-						.routes()
-						.route(r -> r.path("/proxy")
-							.filters(f -> f
-								.setPath("/reservations")
-								.requestRateLimiter(c -> c.setRateLimiter(redisRateLimiter()))
-							)
-							.uri("lb://reservation-service"))
-						.build();
-		}
+	@Bean
+	MapReactiveUserDetailsService authentication() {
+		return new MapReactiveUserDetailsService(
+			User.withDefaultPasswordEncoder()
+				.username("jlong").password("pw").roles("USER").build(),
+			User.withDefaultPasswordEncoder()
+				.username("rwinch").password("pw").roles("USER", "ADMIN").build()
+		);
+	}
 
-		public static void main(String[] args) {
-				SpringApplication.run(ReservationClientApplication.class, args);
-		}
+	@Bean
+	SecurityWebFilterChain authorization(ServerHttpSecurity http) {
+		http.httpBasic();
+		http.csrf().disable();
+		http
+			.authorizeExchange()
+			.pathMatchers("/proxy").authenticated()
+			.anyExchange().permitAll();
+		return http.build();
+	}
+
+	@Bean
+	RedisRateLimiter redisRateLimiter() {
+		return new RedisRateLimiter(5, 7);
+	}
+
+	@Bean
+	RouteLocator gateway(RouteLocatorBuilder rlb) {
+		return
+			rlb
+				.routes()
+				.route(
+					rSpec -> rSpec
+						.host("*.foo.com").and().path("/proxy")
+						.filters(
+							gatewayFilterSpec ->
+								gatewayFilterSpec
+									.addResponseHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
+									.setPath("/reservations")
+									.requestRateLimiter(
+										config -> config
+											.setRateLimiter(redisRateLimiter())
+									)
+						)
+						.uri("lb://reservation-service")
+				)
+				.build();
+	}
+
 }
 
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
 class Reservation {
-		@Id
-		private String id;
-		private String email;
+
+	private String id;
+	private String reservationName;
+}
+
+
+@Component
+class ReservationClient {
+
+	private final ObjectMapper objectMapper;
+
+	private final TcpClientTransport localhost =
+		TcpClientTransport.create("localhost", 7000);
+
+	ReservationClient(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+
+	public Flux<Reservation> getAllReservations() {
+
+		return RSocketFactory
+			.connect()
+			.transport(localhost)
+			.start()
+			.flatMapMany(socket ->
+				socket
+					.requestStream(DefaultPayload.create(new byte[0]))
+					.map(Payload::getDataUtf8)
+					.map(this::from)
+					.doFinally(signal -> socket.dispose())
+			);
+	}
+
+	private Reservation from(String obj) {
+		try {
+			return this.objectMapper
+				.readValue(obj, Reservation.class);
+		}
+		catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
--- finchley/java/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ finchley/java/reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -1,59 +1,149 @@
 package com.example.reservationservice;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.rsocket.AbstractRSocket;
+import io.rsocket.Payload;
+import io.rsocket.RSocketFactory;
+import io.rsocket.SocketAcceptor;
+import io.rsocket.transport.netty.server.TcpServerTransport;
+import io.rsocket.util.DefaultPayload;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.*;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @SpringBootApplication
 public class ReservationServiceApplication {
 
-		@Bean
-		RouterFunction<ServerResponse> routes(ReservationRepository rr) {
-				return route(GET("/reservations"), req -> ok().body(rr.findAll(), Reservation.class));
-		}
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
 
-		@Bean
-		ApplicationRunner run(ReservationRepository rr) {
-				return args ->
-					rr
-						.deleteAll()
-						.thenMany(Flux.just("josh@joshlong.com", "B@b.com", "C@.com", "D@d.com")
-							.map(x -> new Reservation(null, x))
-							.flatMap(rr::save))
-						.thenMany(rr.findAll())
-						.subscribe(System.out::println);
-		}
+	@Bean
+	RouterFunction<ServerResponse> routes(
+		ReservationRepository rr,
+		Environment env) {
+		return route(GET("/reservations"),
+			request -> ok().body(rr.findAll(), Reservation.class))
+			.andRoute(GET("/message"), request -> ok().syncBody(env.getProperty("message")));
+	}
 
-		public static void main(String[] args) {
-				SpringApplication.run(ReservationServiceApplication.class, args);
-		}
 }
 
-interface ReservationRepository extends ReactiveMongoRepository<Reservation, String> {
 
-		Flux<Reservation> findByEmail(String email);
+@Component
+class Runner implements ApplicationRunner {
+
+	private final ReservationRepository reservationRepository;
+
+	Runner(ReservationRepository reservationRepository) {
+		this.reservationRepository = reservationRepository;
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		Flux<String> stringFlux = Flux
+			.just("Josh", "Rada", "Jason", "Ming",
+				"Kieran", "Rudrakshi", "Sean", "Sumi");
+
+		Flux<Reservation> flux = stringFlux
+			.map(name -> new Reservation(null, name))
+			.flatMap(this.reservationRepository::save);
+
+		this.reservationRepository
+			.deleteAll()
+			.thenMany(flux)
+			.thenMany(this.reservationRepository.findAll())
+			.subscribe(System.out::println);
+
+
+	}
 }
 
-@Document
+interface ReservationRepository extends ReactiveMongoRepository<Reservation,
+	String> {
+
+}
+
+
+
+@Log4j2
+@Component
+class ReservationService
+	implements ApplicationListener<ApplicationReadyEvent> {
+
+	private final ReservationRepository reservationRepository;
+	private final ObjectMapper mapper;
+
+	ReservationService(ReservationRepository reservationRepository, ObjectMapper mapper) {
+		this.reservationRepository = reservationRepository;
+		this.mapper = mapper;
+	}
+
+	@Override
+	public void onApplicationEvent(ApplicationReadyEvent event) {
+
+		SocketAcceptor sa =
+			(connectionSetupPayload, rSocket) -> Mono.just(new AbstractRSocket() {
+				@Override
+				public Flux<Payload> requestStream(Payload payload) {
+					return
+						reservationRepository
+							.findAll()
+							.map(x -> {
+								try {
+									return mapper.writeValueAsString(x);
+								}
+								catch (JsonProcessingException e) {
+									throw new RuntimeException(e);
+								}
+							})
+							.map(DefaultPayload::create);
+				}
+			});
+
+		RSocketFactory
+			.receive()
+			.acceptor(sa)
+			.transport(TcpServerTransport.create("localhost", 7000))
+			.start()
+			.onTerminateDetach()
+			.subscribe();
+	}
+}
+
+
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Document
 class Reservation {
-		@Id
-		private String id;
-		private String email;
+
+	@Id
+	private String id;
+	private String reservationName;
 }
